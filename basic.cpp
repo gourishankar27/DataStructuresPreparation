b34c82c579b155ee0d4ac5f4e111258fe59c022c
gourishankar27 <gourishankar2710@gmail.com>
2020-07-11 01:48:31 +0530

    First commit

---
+++ basic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+// cout<< "Hello C++";
+
+                    // Array
+int A[5];
+int B[5] = {1,2,3,4,5};
+
+int i;
+
+                    //Printing of an array
+for(i=0;i<5;i++){
+    cout<<B[i];
+}
+
+
+return 0;
+}
